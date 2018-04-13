01afc9601e4960120b341f44482a87859ca72bb2
vp60132n <vp60132n@pace.edu>
2018-04-13 10:59:28 -0400

    Code for login with RestAPI

--- PacePlace/app/src/main/java/base/pace/paceplace/LoginActivity.java
+++ PacePlace/app/src/main/java/base/pace/paceplace/LoginActivity.java
@@ -1,9 +1,11 @@
 package base.pace.paceplace;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -11,15 +13,31 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import base.pace.paceplace.singup.SignUpActivity;
+import com.loopj.android.http.*;
+/*
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+*/
 
-public class LoginActivity extends AppCompatActivity{
+import org.json.JSONObject;
+
+import base.pace.paceplace.singup.SignUpActivity;
+import base.pace.paceplace.util.PacePlaceConstants;
+import cz.msebera.android.httpclient.Header;
+
+
+public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = "LoginActivity";
 
-    EditText mEmailEditText,mPasswordEditText;
+
+
+    EditText mEmailEditText, mPasswordEditText;
     Button mLoginButton;
     TextView mSignupTextView;
+
+    ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +47,14 @@ public class LoginActivity extends AppCompatActivity{
         configureClickListeners();
     }
 
-    private void configureViews(){
+    private void configureViews() {
         mEmailEditText = findViewById(R.id.emailEditText);
         mPasswordEditText = findViewById(R.id.passwordEditText);
         mLoginButton = findViewById(R.id.loginButton);
         mSignupTextView = findViewById(R.id.signupTextView);
     }
 
-    public void configureClickListeners(){
+    public void configureClickListeners() {
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,7 +66,7 @@ public class LoginActivity extends AppCompatActivity{
                     return;
                 }
 
-                validateUserCredentials(email,password);
+                validateUserCredentials(email, password);
 
                 // To clear the text views
                 mEmailEditText.setText("");
@@ -69,13 +87,57 @@ public class LoginActivity extends AppCompatActivity{
         });
 
     }
-    public void openSignUpActivity(){
+
+    public void openSignUpActivity() {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
-    private void validateUserCredentials(String email, String password){
+
+    private void validateUserCredentials(String email, String password) {
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage(getResources().getString(R.string.please_wait));
+        pDialog.setCancelable(false);
+
+        RequestParams params = new RequestParams();
+        params.put(PacePlaceConstants.EMAIL, email);
+        params.put(PacePlaceConstants.PASSWORD, password);
+        invokeWS(params);
 
     }
+
+    public void invokeWS(RequestParams params) {
+        // Show Progress Dialog
+        pDialog.show();
+        // Make RESTful webservice call using AsyncHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+        Log.e(TAG,"URL : "+PacePlaceConstants.URL_LOGIN);
+        client.post(PacePlaceConstants.URL_LOGIN, params, new JsonHttpResponseHandler() {
+
+
+            // When the response returned by REST has Http response code '200'
+            @Override
+            //public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject jsonObject) {
+                // called when response HTTP status is "200 OK"
+                Log.e(TAG,"SSSSSSSSSSSSS");
+                Log.e(TAG,"SSSSSSSSSSSSS : "+jsonObject);
+                generateToastMessage(R.string.login_success);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+            //public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                generateToastMessage(R.string.login_failed);
+                Log.e(TAG,"EEEEEEEE :: "+ PacePlaceConstants.URL_LOGIN);
+                Log.e(TAG,"EEEEEEEE : "+statusCode);
+                Log.e(TAG,"EEEEEEEE : "+errorResponse);
+                throwable.printStackTrace();
+            }
+        });
+        pDialog.hide();
+    }
+
 
     // To generate Toast message
     private void generateToastMessage(int id) {
---
+++ PacePlace/app/src/main/java/base/pace/paceplace/util/PacePlaceConstants.java
@@ -0,0 +1,11 @@
+package base.pace.paceplace.util;
+
+public abstract class PacePlaceConstants {
+
+    //public static final String URL = "http://192.168.172.1:5000/";
+    public static final String URL = "http://ec2-18-218-139-175.us-east-2.compute.amazonaws.com:5000/";
+    public static final String URL_LOGIN = URL + "getUserInfo/";
+    public static final String EMAIL = "EMAIL";
+    public static final String PASSWORD = "PASSWORD";
+
+}
