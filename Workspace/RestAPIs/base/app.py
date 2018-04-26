88d72a5dd5abb3e380dad7ea70fcbcda1e6eff42
richiescarvalho <richie.carvalho0910@gmail.com>
2018-04-26 00:37:34 -0400

    Merged Code

--- Workspace/RestAPIs/base/app.py
+++ Workspace/RestAPIs/base/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from flaskext.mysql import MySQL
 from flask_restful import Api
+from flaskext.mysql import MySQL
 from base.paceplace.common import db
 
 mysql = MySQL()
 app = Flask(__name__)
-# cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.config['MYSQL_DATABASE_USER'] = db.MYSQL_DATABASE_USER
 app.config['MYSQL_DATABASE_PASSWORD'] = db.MYSQL_DATABASE_PASSWORD
@@ -16,7 +15,9 @@ mysql.init_app(app)
 api = Api(app)
 
 # importing other python files which will not be used in this file, we import them just to initialize the file data
-# static_info has to be the first line of the import statement -- rest all will follow it
-
+### static_info has to be the first line of the import statement -- rest all will follow it
 from base.paceplace.common import static_info
+
 from base.paceplace.restapi import user_info
+from base.paceplace.restapi import course_info
+
